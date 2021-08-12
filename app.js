09cdf692c1979322494467e82f313cc3b5485a32
ProsperieEli <ElijahProsperie@gmail.com>
2021-08-11 20:02:38 -0400

    Made new feature to replace name

---
+++ app.js
@@ -0,0 +1,15 @@
+const myInput = document.getElementById('my-input');
+
+const myButton = document.getElementById('my-button');
+myButton.addEventListener('click', () => {
+    myDisplay.textContent = myInput.value;
+})
+
+
+const myDisplay = document.getElementById('my-injection-space');
+
+
+
+
+
+
